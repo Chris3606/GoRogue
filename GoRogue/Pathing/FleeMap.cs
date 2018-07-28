a1b86065bd5337877fbecd8a02b31a63ed88cd72
Chris Ridley <cridley106@gmail.com>
2018-07-28 14:09:55 -0500

    Code cleanup, removed debug leftovers, etc.

--- GoRogue/Pathing/FleeMap.cs
+++ GoRogue/Pathing/FleeMap.cs
@@ -27,96 +27,75 @@ namespace GoRogue.Pathing
         public FleeMap(GoalMap baseMap, double magnitude)
         {
             _baseMap = baseMap ?? throw new ArgumentNullException(nameof(baseMap));
-            this.Magnitude = magnitude;
+            Magnitude = magnitude;
             _goalMap = new ArrayMap<double?>(baseMap.Width, baseMap.Height);
             _nodes = new ArrayMap<PositionNode>(baseMap.Width, baseMap.Height);
             foreach (var pos in _nodes.Positions())
                 _nodes[pos] = new PositionNode(pos);
 
-            _baseMap.Updated += this.Update;
+            _baseMap.Updated += Update;
         }
         public FleeMap(GoalMap baseMap) : this(baseMap, 1.2)
         {
         }
 
         /// <summary>
-        /// Returns the goal-map values represented as a 2D grid-style string.
+        /// Returns the flee-map values represented as a 2D grid-style string.
         /// </summary>
-        /// <returns>A string representing the goal map values.</returns>
+        /// <returns>A string representing the flee map values.</returns>
         public override string ToString() =>
             _goalMap.ToString(val => val.HasValue ? val.Value.ToString() : "null");
 
         /// <summary>
-        /// Returns the goal-map values represented as a 2D-grid-style string, where any value that
+        /// Returns the flee-map values represented as a 2D-grid-style string, where any value that
         /// isn't null is formatted as per the specified format string.
         /// </summary>
         /// <param name="formatString">Format string to use for non-null values.</param>
-        /// <returns>A string representing the goal-map values.</returns>
+        /// <returns>A string representing the flee-map values.</returns>
         public string ToString(string formatString) =>
             _goalMap.ToString(val => val.HasValue ? val.Value.ToString(formatString) : "null");
 
         /// <summary>
-        /// Returns the goal-map values represented as a 2D-grid-style string, with the given field size.
+        /// Returns the flee-map values represented as a 2D-grid-style string, with the given field size.
         /// </summary>
         /// <param name="fieldSize">Number of characters allocated to each value in the string.</param>
-        /// <returns>A string representing the goal-map values.</returns>
+        /// <returns>A string representing the flee-map values.</returns>
         public string ToString(int fieldSize)
             => _goalMap.ToString(fieldSize);
 
         /// <summary>
-        /// Returns the goal-map values represented as a 2D-grid-style string, with the given field
+        /// Returns the flee-map values represented as a 2D-grid-style string, with the given field
         /// size, and any non-null values formatted using the given format string.
         /// </summary>
         /// <param name="fieldSize">Number of characters allocated to each value in the string.</param>
         /// <param name="formatString">Format string to use for non-null values.</param>
-        /// <returns>A string representing the goal-map values.</returns>
+        /// <returns>A string representing the flee-map values.</returns>
         public string ToString(int fieldSize, string formatString)
             => _goalMap.ToString(fieldSize, val => val.HasValue ? val.Value.ToString(formatString) : "null");
 
         private void Update()
         {
             var adjacencyRule = (AdjacencyRule)_baseMap.DistanceMeasurement;
-
-            // var openSet = new HashSet<Coord>();
-
             var openSet = new GenericPriorityQueue<PositionNode, double>(Width * Height);
+
             foreach (var point in _baseMap.Walkable)
             {
                 var newPoint = _baseMap[point] * -Magnitude;
                 _goalMap[point] = newPoint;
-                //openSet.Add(point);
-                //if (!openSet.EnqueueWithoutDuplicates(point, newPoint.Value))
-                //    throw new Exception("Duplicate item enqueued.");
-                if (!openSet.Contains(_nodes[point]))
-                    openSet.Enqueue(_nodes[point], newPoint.Value);
-                // else
-                //    throw new Exception("Duplicate item attempted to be enqueued.");
+
+                openSet.Enqueue(_nodes[point], newPoint.Value);
             }
             var edgeSet = new HashSet<Coord>();
             var closedSet = new HashSet<Coord>();
-            // var walkable = new HashSet<Coord>(_baseMap.Walkable);
+
             while (openSet.Count > 0) //multiple runs are needed to deal with islands
             {
-                /*
-                var min = (double)Width * Height;
-                var minPoint = Coord.Get(-1, -1);
-                foreach (var point in openSet)
-                {
-                    var value = _goalMap[point];
-                    if (value < min)
-                    {
-                        min = value.Value;
-                        minPoint = point;
-                    }
-                }
-                */
                 var minNode = openSet.Dequeue();
                 closedSet.Add(minNode.Position);
-                // openSet.Remove(minPoint);
 
                 foreach (var openPoint in adjacencyRule.Neighbors(minNode.Position))
                 {
-                    if ((!closedSet.Contains(openPoint)) && _baseMap.BaseMap[openPoint] != GoalState.Obstacle) // walkable.Contains(openPoint))
+                    if ((!closedSet.Contains(openPoint)) && _baseMap.BaseMap[openPoint] != GoalState.Obstacle)
                         edgeSet.Add(openPoint);
                 }
                 while (edgeSet.Count > 0)
@@ -126,7 +105,7 @@ namespace GoRogue.Pathing
                         var current = _goalMap[coord].Value;
                         foreach (var openPoint in adjacencyRule.Neighbors(coord))
                         {
-                            if (closedSet.Contains(openPoint) || _baseMap.BaseMap[openPoint] == GoalState.Obstacle) // !walkable.Contains(openPoint))
+                            if (closedSet.Contains(openPoint) || _baseMap.BaseMap[openPoint] == GoalState.Obstacle)
                                 continue;
                             var neighborValue = _goalMap[openPoint].Value;
                             var newValue = current + _baseMap.DistanceMeasurement.Calculate(coord, openPoint);
@@ -142,7 +121,6 @@ namespace GoRogue.Pathing
                         openSet.Remove(_nodes[coord]);
                     }
                 }
-                // openSet.RemoveWhere(c => closedSet.Contains(c));
             }
         }
         #region IDisposable Support
@@ -156,7 +134,7 @@ namespace GoRogue.Pathing
         {
             if (!_disposed)
             {
-                _baseMap.Updated -= this.Update;
+                _baseMap.Updated -= Update;
                 _disposed = true;
                 GC.SuppressFinalize(this);
             }
