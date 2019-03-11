1ca16b2cb654608bf98174fb53bce351fbce3490
Chris3606 <cridley106@gmail.com>
2019-03-11 10:52:54 -0500

    Updated docstrings for BoundedRectangle.

--- GoRogue/BoundedRectangle.cs
+++ GoRogue/BoundedRectangle.cs
@@ -1,8 +1,9 @@
 ﻿namespace GoRogue
 {
 	/// <summary>
-	/// A class that contains a rectangle Area that is automatically "locked" to being inside the
-	/// BoundingBox. A typical use might be keeping track of a camera view area.
+	/// This class defines a rectanglar area, whose position is automatically "locked" to
+	/// being inside a rectangular bounding box as it is changed. A typical use might be
+	/// keeping track of a camera's view area.
 	/// </summary>
 	public class BoundedRectangle
 	{
@@ -10,10 +11,10 @@
 		private Rectangle _boundingBox;
 
 		/// <summary>
-		/// Constructor. Takes the initial area and bounding box.
+		/// Constructor.
 		/// </summary>
-		/// <param name="area">Initial value for Area.</param>
-		/// <param name="boundingBox">Initial bounding box for Area.</param>
+		/// <param name="area">Initial area for the rectangle.</param>
+		/// <param name="boundingBox">Initial bounding box by which to bound the rectangle.</param>
 		public BoundedRectangle(Rectangle area, Rectangle boundingBox)
 		{
 			_boundingBox = boundingBox;
@@ -23,10 +24,9 @@
 		}
 
 		/// <summary>
-		/// The rectangle that is guaranteed to be contained within the BoundingBox. Although it does
-		/// not specifically provide a set accessor, this property is returning a reference and as
-		/// such may be assigned to. Whenever the rectangle is retrieved, if it is not contained
-		/// within BoundingBox it will be modified as necessary to keep it within those bounds.
+		/// The rectangle that is guaranteed to be contained completely within <see cref="BoundingBox"/>.
+		/// Although it does not specifically provide a set accessor, this property is returning a
+		/// reference and as such may be assigned to.
 		/// </summary>
 		public ref Rectangle Area
 		{
@@ -41,9 +41,9 @@
 		}
 
 		/// <summary>
-		/// The rectangle which Area is automatically bounded to. Similar to area, although this
-		/// property does not explicitly provide a set accessor, it is returning a reference so the
-		/// property may be assigned to.
+		/// The rectangle which <see cref="Area"/> is automatically bounded to be within.  Although this
+		/// property does not explicitly provide a set accessor, it is returning a reference so therefore
+		/// the property may be assigned to.
 		/// </summary>
 		public ref Rectangle BoundingBox
 		{
