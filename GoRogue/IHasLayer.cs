5e7a8e3c2b4b9b994f4b1efcca733e551ca32a64
Chris3606 <cridley106@gmail.com>
2019-03-12 14:18:31 -0500

    Updated docstrings for IHasLayer.

--- GoRogue/IHasLayer.cs
+++ GoRogue/IHasLayer.cs
@@ -1,15 +1,18 @@
 ﻿namespace GoRogue
 {
 	/// <summary>
-	/// Interface to be implemented by objects that will be used in LayeredSpatialMap/Map classes.
+	/// Interface to be implemented by objects that will be added to a <see cref="LayeredSpatialMap{T}"/>.
 	/// </summary>
 	public interface IHasLayer
 	{
 		/// <summary>
 		/// The layer on which the object should reside. Higher numbers indicate layers closer to the
-		/// "top". This is assumed to remain constant while the object is within a data structure
-		/// that uses this interface, if it is modified, that data structure will become out of sync.
+		/// "top".
 		/// </summary>
+		/// <remarks>
+		/// This value is assumed to remain constant while the object is within a data structure
+		/// that uses this interface -- if it is modified, that data structure will become out of sync.
+		/// </remarks>
 		int Layer { get; }
 	}
 }
