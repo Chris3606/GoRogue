9d6fdad21b0fb95127ede83d88c590ed401ab326
Chris3606 <cridley106@gmail.com>
2019-03-12 10:47:37 -0500

    Updated docstrings for IReadOnlyDisjointSet.

--- GoRogue/IReadOnlyDisjointSet.cs
+++ GoRogue/IReadOnlyDisjointSet.cs
@@ -1,8 +1,7 @@
 ﻿namespace GoRogue
 {
 	/// <summary>
-	/// Basic read-only representation of a Disjoint set data structure. Assumes it is holding
-	/// integers between 0 and size - 1.
+	/// Read-only representation of <see cref="DisjointSet"/>
 	/// </summary>
 	public interface IReadOnlyDisjointSet
 	{
@@ -12,17 +11,17 @@
 		int Count { get; }
 
 		/// <summary>
-		/// Returns the parent of the set containing obj, performing path compression as search is completed.
+		/// Returns the parent of the set containing <paramref name="obj"/>, performing path compression as search is completed.
 		/// </summary>
 		/// <param name="obj">Object to search for.</param>
-		/// <returns>The parent of the obj given.</returns>
+		/// <returns>The parent of the object given.</returns>
 		int Find(int obj);
 
 		/// <summary>
 		/// Returns true if the two objects specified are in the same set.
 		/// </summary>
-		/// <param name="obj1">First object.</param>
-		/// <param name="obj2">Second object.</param>
+		/// <param name="obj1"/>
+		/// <param name="obj2"/>
 		/// <returns>True if the two objects are in the same set, false otherwise.</returns>
 		bool InSameSet(int obj1, int obj2);
 	}
