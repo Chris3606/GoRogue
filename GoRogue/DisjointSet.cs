49cfe44724e61998595ec817393e36995e3c064f
Chris3606 <cridley106@gmail.com>
2019-03-12 10:44:09 -0500

    Update docstrings for DisjointSet.

--- GoRogue/DisjointSet.cs
+++ GoRogue/DisjointSet.cs
@@ -3,9 +3,14 @@
 namespace GoRogue
 {
 	/// <summary>
-	/// Basic representation of a Disjoint set data structure. Assumes it is holding integers between
-	/// 0 and size - 1.
+	/// Basic representation of a disjoint set data structure.
 	/// </summary>
+	/// <remarks>
+	/// For reasons pertaining to optimization, this disjoint set implementation does not use
+	/// generics, and instead holds integer values, which MUST be exactly all integer values in range
+	/// [0, num_items_in_set - 1].  Thus, you will need to assign appropriate IDs to objects you intend
+	/// to add and map them appropriately.
+	/// </remarks>
 	public class DisjointSet : IReadOnlyDisjointSet
 	{
 		private int[] parents;
@@ -13,7 +18,7 @@ namespace GoRogue
 		private int[] sizes;
 
 		/// <summary>
-		/// Constructor. Size of disjoint set is specified -- it holds values between 0 and size - 1.
+		/// Constructor. The disjoint set will contain all values in range [0, <paramref name="size"/> - 1].
 		/// </summary>
 		/// <param name="size">(Max) size of the disjoint set.</param>
 		public DisjointSet(int size)
@@ -41,10 +46,11 @@ namespace GoRogue
 		public IReadOnlyDisjointSet AsReadOnly() => this;
 
 		/// <summary>
-		/// Returns the parent of the set containing obj, performing path compression as search is completed.
+		/// Returns the parent of the set containing <paramref name="obj"/>, performing path compression
+		/// as the search is completed.
 		/// </summary>
 		/// <param name="obj">Object to search for.</param>
-		/// <returns>The parent of the obj given.</returns>
+		/// <returns>The parent of the object given.</returns>
 		public int Find(int obj)
 		{
 			// Find base parent, and path compress
@@ -57,8 +63,8 @@ namespace GoRogue
 		/// <summary>
 		/// Returns true if the two objects specified are in the same set.
 		/// </summary>
-		/// <param name="obj1">First object.</param>
-		/// <param name="obj2">Second object.</param>
+		/// <param name="obj1"/>
+		/// <param name="obj2"/>
 		/// <returns>True if the two objects are in the same set, false otherwise.</returns>
 		public bool InSameSet(int obj1, int obj2)
 		{
@@ -70,8 +76,8 @@ namespace GoRogue
 		/// every element in the sets containing the two objects specified will be part of one larger set.
 		/// </summary>
 		/// <remarks>If the two elements are already in the same set, nothing is done.</remarks>
-		/// <param name="obj1">First object.</param>
-		/// <param name="obj2">Second object.</param>
+		/// <param name="obj1"/>
+		/// <param name="obj2"/>
 		public void MakeUnion(int obj1, int obj2)
 		{
 			int i = Find(obj1);
