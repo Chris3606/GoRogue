e2cfacdd4d41d34b1f59cd02b11c40c3065be52f
Chris3606 <cridley106@gmail.com>
2019-03-12 10:28:28 -0500

    Update docstrings for Distance. Minor param rename

--- GoRogue/Distance.cs
+++ GoRogue/Distance.cs
@@ -3,36 +3,36 @@
 namespace GoRogue
 {
 	/// <summary>
-	/// Class representing a method of calculating distance. You cannot create instances of this
+	/// Class representing methods of calculating distance on a grid. You cannot create instances of this
 	/// class using a constructor -- instead this class contains static instances representing the
-	/// applicable types of distance calculations.
+	/// various distance calculations.
 	/// </summary>
 	/// <remarks>
 	/// Provides functions that calculate the distance between two points according to the distance
-	/// measurement being used. Distance instances can also be explicitly casted to Radius instances
-	/// -- the 2D radius shape that corresponds to the shape of a "radius" according to the casted
-	/// distance calculation is retrieved (eg, EUCLIDEAN casts to CIRCLE, MANHATTAN to DIAMOND, etc.)
+	/// measurement being used. Instances of Distance are also implicitly convertible to <see cref="AdjacencyRule"/>
+	/// types (since an adjacency method is implied by a distance calculation), and they may also be explicitly
+	/// casted to the appropriate 2D <see cref="Radius"/> types.
 	/// </remarks>
 	public class Distance
 	{
 		/// <summary>
-		/// CHEBYSHEV distance (equivalent to 8-way movement with no extra cost for diagonals).
+		/// Represents chebyshev distance (equivalent to 8-way movement with no extra cost for diagonals).
 		/// </summary>
 		public static Distance CHEBYSHEV = new Distance(Types.CHEBYSHEV);
 
 		/// <summary>
-		/// EUCLIDEAN distance (equivalent to 8-way movement with extra cost for diagonals).
+		/// Represents euclidean distance (equivalent to 8-way movement with ~1.41 movement cost for diagonals).
 		/// </summary>
 		public static Distance EUCLIDEAN = new Distance(Types.EUCLIDEAN);
 
 		/// <summary>
-		/// MANHATTAN distance (equivalent to 4-way, cardinal-only movement).
+		/// Represents manhattan distance (equivalent to 4-way, cardinal-only movement).
 		/// </summary>
 		public static Distance MANHATTAN = new Distance(Types.MANHATTAN);
 
 		/// <summary>
-		/// Enum type for the distance calculation. Useful for conducting a switch statement on
-		/// distance instances.
+		/// Enum value representing the method of calcuating distance -- useful for using
+		/// Distance types in switch statements.
 		/// </summary>
 		public readonly Types Type;
 
@@ -44,32 +44,36 @@ namespace GoRogue
 		}
 
 		/// <summary>
-		/// Enum for types used under the hood in Direction classes -- allows for convenient switch
-		/// statements to be used with respect to directions.
+		/// Enum representing Distance types. Each Distance instance has a <see cref="Type"/> field
+		/// which contains the corresponding value from this enum.  Useful for easy mapping of Distance
+		/// types to a primitive type (for cases like a switch statement).
 		/// </summary>
 		public enum Types
 		{
 			/// <summary>
-			/// Enum type for Distance.MANHATTAN.
+			/// Enum type for <see cref="Distance.MANHATTAN"/>.
 			/// </summary>
 			MANHATTAN,
 
 			/// <summary>
-			/// Enum type for Distance.EUCLIDEAN.
+			/// Enum type for <see cref="Distance.EUCLIDEAN"/>.
 			/// </summary>
 			EUCLIDEAN,
 
 			/// <summary>
-			/// Enum type for Distance.CHEBYSHEV.
+			/// Enum type for <see cref="Distance.CHEBYSHEV"/>.
 			/// </summary>
 			CHEBYSHEV
 		};
 
 		/// <summary>
-		/// Allows explicit casting to Radius type. The 2D radius shape corresponding to the
-		/// definition of a radius according to the distance calculation casted will be retrieved.
+		/// Allows explicit casting to <see cref="Radius"/> type.
 		/// </summary>
-		/// <param name="distance">Distance type being casted.</param>
+		/// <remarks>
+		/// The 2D radius shape corresponding to the definition of a radius according to the distance calculation
+		/// casted will be returned.
+		/// </remarks>
+		/// <param name="distance"/>
 		public static explicit operator Radius(Distance distance)
 		{
 			switch (distance.Type)
@@ -89,10 +93,13 @@ namespace GoRogue
 		}
 
 		/// <summary>
-		/// Allows implicit casting to the AdjacencyRule type. The adjacency rule corresponding to
-		/// the definition of a radius according to the distance calculation casted will be retrieved.
+		/// Allows implicit casting to the <see cref="AdjacencyRule"/> type. 
 		/// </summary>
-		/// <param name="distance">Distance type being casted.</param>
+		/// <remarks>
+		/// The adjacency rule corresponding to the definition of a adjacency according to the
+		/// distance calculation casted will be returned.
+		/// </remarks>
+		/// <param name="distance"/>
 		public static implicit operator AdjacencyRule(Distance distance)
 		{
 			switch (distance.Type)
@@ -112,8 +119,8 @@ namespace GoRogue
 		/// <summary>
 		/// Gets the Distance class instance representing the distance type specified.
 		/// </summary>
-		/// <param name="distanceType">The enum value for the distance method.</param>
-		/// <returns>The distance class representing the given distance calculation.</returns>
+		/// <param name="distanceType">The enum value for the distance calculation method.</param>
+		/// <returns>The Distance class representing the given distance calculation.</returns>
 		public static Distance ToDistance(Types distanceType)
 		{
 			switch (distanceType)
@@ -148,8 +155,7 @@ namespace GoRogue
 		}
 
 		/// <summary>
-		/// Returns the distance between the two (3D) points specified. Points are floating point
-		/// instead of integer-values.
+		/// Returns the distance between the two (3D) points specified.
 		/// </summary>
 		/// <param name="startX">X-Coordinate of the starting point.</param>
 		/// <param name="startY">Y-Coordinate of the starting point.</param>
@@ -185,8 +191,7 @@ namespace GoRogue
 		public double Calculate(Coord start, Coord end) => Calculate((double)start.X, (double)start.Y, (double)end.X, (double)end.Y);
 
 		/// <summary>
-		/// Returns the distance between the two (2D) points specified. Points are floating point
-		/// instead of integer-values.
+		/// Returns the distance between the two (2D) points specified.
 		/// </summary>
 		/// <param name="startX">X-Coordinate of the starting point.</param>
 		/// <param name="startY">Y-Coordinate of the starting point.</param>
@@ -202,28 +207,27 @@ namespace GoRogue
 
 		/// <summary>
 		/// Returns the distance between two locations, given the change in X and change in Y value
-		/// (specified by the X and Y values of the coordinate.
+		/// (specified by the X and Y values of the given vector).
 		/// </summary>
 		/// <param name="end">The delta-x and delta-y between the two locations.</param>
 		/// ///
-		/// <returns>The distance between the two locations.</returns>
-		public double Calculate(Coord end) => Calculate((double)end.X, (double)end.Y);
+		/// <returns>The distance between two locations withe the given delta-change values.</returns>
+		public double Calculate(Coord deltaChange) => Calculate((double)deltaChange.X, (double)deltaChange.Y);
 
 		/// <summary>
 		/// Returns the distance between two locations, given the change in X and change in Y value.
 		/// </summary>
 		/// <param name="dx">The delta-x between the two locations.</param>
 		/// <param name="dy">The delta-y between the two locations.</param>
-		/// <returns>The distance between the two locations.</returns>
+		/// <returns>The distance between two locations with the given delta-change values.</returns>
 		public double Calculate(int dx, int dy) => Calculate((double)dx, (double)dy);
 
 		/// <summary>
 		/// Returns the distance between two locations, given the change in X and change in Y value.
-		/// The change in X and Y are specified as floating point values rather than integers.
 		/// </summary>
 		/// <param name="dx">The delta-x between the two locations.</param>
 		/// <param name="dy">The delta-y between the two locations.</param>
-		/// <returns>The distance between the two locations.</returns>
+		/// <returns>The distance between two locations with the given delta-change values.</returns>
 		public double Calculate(double dx, double dy) => Calculate(dx, dy, 0);
 
 		/// <summary>
@@ -232,17 +236,16 @@ namespace GoRogue
 		/// <param name="dx">The delta-x between the two locations.</param>
 		/// <param name="dy">The delta-y between the two locations.</param>
 		/// <param name="dz">The delta-z between the two locations.</param>
-		/// <returns>The distance between the two locations.</returns>
+		/// <returns>The distance between the locations with the given delta-change values.</returns>
 		public double Calculate(int dx, int dy, int dz) => Calculate((float)dx, (float)dy, (float)dz);
 
 		/// <summary>
-		/// Returns the distance between two locations, given the change in X, Y, and Z value. The
-		/// change in X, Y, and Z are specified as floating point values rather than integers.
+		/// Returns the distance between two locations, given the change in X, Y, and Z value.
 		/// </summary>
 		/// <param name="dx">The delta-x between the two locations.</param>
 		/// <param name="dy">The delta-y between the two locations.</param>
 		/// <param name="dz">The delta-z between the two locations.</param>
-		/// <returns>The distance between the two locations.</returns>
+		/// <returns>The distance between two locations with the given delta-change values.</returns>
 		public double Calculate(double dx, double dy, double dz)
 		{
 			dx = Math.Abs(dx);
@@ -268,9 +271,9 @@ namespace GoRogue
 		}
 
 		/// <summary>
-		/// Returns a string representation of the Distance.
+		/// Returns a string representation of the distance calculation method represented.
 		/// </summary>
-		/// <returns>A string representation of the Distance.</returns>
+		/// <returns>A string representation of the distance method represented.</returns>
 		public override string ToString() => writeVals[(int)Type];
 	}
 }
