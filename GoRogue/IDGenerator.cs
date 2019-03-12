55054bc6d84a97b864e09a07cdcb425aa1f1130e
Chris3606 <cridley106@gmail.com>
2019-03-12 14:08:34 -0500

    Updated docstrings for IDGenerator.

--- GoRogue/IDGenerator.cs
+++ GoRogue/IDGenerator.cs
@@ -2,12 +2,14 @@
 {
 	/// <summary>
 	/// Class designed as a helper for situations where you need to generate and assign a unique
-	/// integer to each instance of a class, eg. for a class implementing IHasID (see that interface
-	/// documentation for usage example). One may initialize it with a starting unsigned integer, or
-	/// 0 if none is specified. Then, every time one wants to use an integer, one should call
-	/// UseID(), and use the one it returns. It is not thread-safe on its own -- if it needs to be,
-	/// one might consider using a lock around any UseID calls.
+	/// integer to each instance of a class, eg. for a class implementing <see cref="IHasID"/>.
 	/// </summary>
+	/// <remarks>
+	/// The class may be initialized with a starting unsigned integer -- if none is given, 0 is the default
+	/// starting point. To assign an ID, call <see cref="UseID"/>, and assign the value that it returns.
+	/// This class is NOT thread-safe on its own -- if it needs to be, you can simply use a lock to wrap
+	/// any calls to UseID.
+	/// </remarks>
 	public class IDGenerator
 	{
 		private uint currentInteger;
@@ -18,8 +20,8 @@
 		/// Constructor.
 		/// </summary>
 		/// <param name="startingInt">
-		/// Unsigned integer to start at (one that will be returned first time UseID is called).
-		/// Default 0.
+		/// Unsigned integer to start at (one that will be returned first time <see cref="UseID"/> is called).
+		/// Defaults to 0.
 		/// </param>
 		public IDGenerator(uint startingInt = 0)
 		{
@@ -29,7 +31,7 @@
 
 		/// <summary>
 		/// Call every time you wish to "assign" an ID. The integer returned will never be returned
-		/// again (each integer will be unique, per instance of this class).
+		/// again (each integer returned by this function will be unique).
 		/// </summary>
 		/// <returns>The ID that has been assigned.</returns>
 		public uint UseID()
