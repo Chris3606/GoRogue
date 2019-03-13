7b5b9c85398d7b29d87392b120a82ee750d6921d
Chris3606 <cridley106@gmail.com>
2019-03-13 12:30:21 -0500

    Changed docstrings for MathHelpers.

--- GoRogue/MathHelpers.cs
+++ GoRogue/MathHelpers.cs
@@ -19,7 +19,7 @@ namespace GoRogue
 		/// </summary>
 		/// <param name="number">Number to round.</param>
 		/// <param name="toMultipleOf">Number given is rounded up to nearest multiple of this number.</param>
-		/// <returns>The number parameter, rouded up to the nearest multiple of toMultipleOf.</returns>
+		/// <returns>The number parameter, rouded up to the nearest multiple of <paramref name="toMultipleOf"/>.</returns>
 		public static int RoundToMultiple(int number, int toMultipleOf)
 		{
 			int isPositive = (number >= 0) ? 1 : 0;
@@ -42,18 +42,22 @@ namespace GoRogue
 
 		// Basically modulo for array indices, solves - num issues. (-1, 3) is 2.
 		/// <summary>
-		/// A modified modulo operator. Computes similar to num % wrapTo, following the formula (num
-		/// % wrapTo + wrapTo) % wrapTo. Practically it differs from regular modulo in that the
-		/// values it returns when negative values for num are given wrap around like one would want
-		/// an array index to (if wrapTo is list.length, -1 wraps to list.length - 1). For example, 0
-		/// % 3 = 0, -1 % 3 = -1, -2 % 3 = -2, -3 % 3 = 0, and so forth, but WrapTo(0, 3) = 0,
-		/// WrapTo(-1, 3) = 2, -2 % 3 = 1, -3 % 3 = 0, and so forth. This can be useful if you're
+		/// A modified modulo operator, which practically differs from <paramref name="num"/> / <paramref name="wrapTo"/>
+		/// in that it wraps from 0 to <paramref name="wrapTo"/> - 1, as well as from <paramref name="wrapTo"/> - 1 to 0.
+		/// </summary>
+		/// <remarks>
+		/// A modified modulo operator. Returns the result of  the formula
+		/// (<paramref name="num"/> % <paramref name="wrapTo"/> + <paramref name="wrapTo"/>) % <paramref name="wrapTo/>.
+		/// 
+		/// Practically it differs from regular modulo in that the values it returns when negative values for <paramref name="num"/>
+		/// are wrapped around like one would want an array index to (if wrapTo is list.length, -1 wraps to list.length - 1). For example,
+		/// 0 % 3 = 0, -1 % 3 = -1, -2 % 3 = -2, -3 % 3 = 0, and so forth, but WrapTo(0, 3) = 0,
+		/// WrapTo(-1, 3) = 2, WrapTo(-2, 3) = 1, WrapTo(-3, 3) = 0, and so forth. This can be useful if you're
 		/// trying to "wrap" a number around at both ends, for example wrap to 3, such that 3 wraps
-		/// to 0, and 0 wraps to 2. This is common if you are wrapping around an array index to the
+		/// to 0, and -1 wraps to 2. This is common if you are wrapping around an array index to the
 		/// length of the array and need to ensure that positive numbers greater than or equal to the
 		/// length of the array wrap to the beginning of the array (index 0), AND that negative
-		/// numbers (under 0) wrap around to the end of the array (Length - 1). In that example to
-		/// wrap a number i you would call WrapTo(i, array.Length).
+		/// numbers (under 0) wrap around to the end of the array (Length - 1).
 		/// </summary>
 		/// <param name="num">The number to wrap.</param>
 		/// <param name="wrapTo">
