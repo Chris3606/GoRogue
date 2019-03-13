403d905a3b31f987a3b6f748d99979db6c5fd024
Chris3606 <cridley106@gmail.com>
2019-03-13 13:44:44 -0500

    Updated docstrings for RadiusAreaProvider.

--- GoRogue/RadiusAreaProvider.cs
+++ GoRogue/RadiusAreaProvider.cs
@@ -3,18 +3,19 @@
 namespace GoRogue
 {
 	/// <summary>
-	/// Class capable of getting all unique positions inside of a given radius and (optional) bounds.
+	/// Class containing functions capable of returning all unique positions inside of a given radius
+	/// and (optional) bounds.
 	/// </summary>
 	/// <remarks>
-	/// In the case that MANHATTAN/CHEBYSHEV distance, or DIAMOND/SQUARE/OCTAHEDRON/CUBE shapes are
-	/// used, Coords are guaranteed to be returned in order of distance from the center, from least
-	/// to greatest. This guarantee does NOT hold if EUCLIDEAN distance, or CIRCLE/SPHERE radius
-	/// shapes are specified. /// If no bounds are specified, the IEnumerable returned by positions
-	/// will contain each coordinate within the radius. Otherwise, it will contain each coordinate in
-	/// the radius that is also within the bounds of the rectangle. /// If the same radius length is
-	/// being used multiple times (even from different center points), it is recommended to use only
-	/// one RadiusAreaProvider, as the class allocates measurable memory, and using only one instance
-	/// if the radius is used multiple times prevents reallocation. /// When the Radius value is
+	/// In the case that MANHATTAN/CHEBYSHEV distance, or DIAMOND/SQUARE/OCTAHEDRON/CUBE radius shapes
+	/// are used, positions returned are guaranteed to be returned in order of distance from the center,
+	/// from least to greatest. This guarantee does NOT hold if EUCLIDEAN distance, or CIRCLE/SPHERE radius
+	/// shapes are specified. If no bounds are specified, the IEnumerable returned by the
+	/// <see cref="CalculatePositions" function will contain each coordinate within the radius. Otherwise, it
+	/// will contain each coordinate in the radius that is also within the bounds of the rectangle. If the same
+	/// radius length is being used multiple times (even from different center points), it is recommended to use
+	/// only one RadiusAreaProvider, as the class allocates measurable memory, and using only one instance
+	/// if the radius is used multiple times prevents reallocation. When the Radius value is
 	/// changed, reallocation must be performed, however the overhead should be insignificant on
 	/// everything but extremely large radiuses.
 	/// </remarks>
@@ -29,16 +30,15 @@ namespace GoRogue
 		private Coord topLeft;
 
 		/// <summary>
-		/// Constructor. Specifies center, radius length, distance calculation that defines the
-		/// concept of radius, and bounds.
+		/// Constructor.
 		/// </summary>
 		/// <param name="center">The center point of the radius.</param>
 		/// <param name="radius">The length of the radius.</param>
 		/// <param name="distanceCalc">
 		/// The distance calculation used to determine what shape the radius has (or a type
-		/// implicitly convertible to Distance, eg. Radius)..
+		/// implicitly convertible to a distance method, eg. <see cref="Radius"/>).
 		/// </param>
-		/// <param name="bounds">The bounds to constrain the returned Coords to.</param>
+		/// <param name="bounds">The bounds to constrain the returned locations to.</param>
 		public RadiusAreaProvider(Coord center, int radius, Distance distanceCalc, Rectangle bounds)
 		{
 			_center = center;
@@ -52,50 +52,47 @@ namespace GoRogue
 		}
 
 		/// <summary>
-		/// Constructor. Specifies center, radius length, distance calculation that defines the
-		/// concept of radius, and bounds.
+		/// Constructor.
 		/// </summary>
 		/// <param name="centerX">The x-value of the center point of the radius.</param>
 		/// <param name="centerY">The y-value of the center point of the radius.</param>
 		/// <param name="radius">The length of the radius.</param>
 		/// <param name="distanceCalc">
 		/// The distance calculation used to determine what shape the radius has (or a type
-		/// implicitly convertible to Distance, eg. Radius)..
+		/// implicitly convertible to a distance method, eg. <see cref="Radius"/>).
 		/// </param>
-		/// <param name="bounds">The bounds to constrain the returned Coords to.</param>
+		/// <param name="bounds">The bounds to constrain the returned locations to.</param>
 		public RadiusAreaProvider(int centerX, int centerY, int radius, Distance distanceCalc, Rectangle bounds)
 			: this(new Coord(centerX, centerY), radius, distanceCalc, bounds) { }
 
 		/// <summary>
-		/// Constructor. Specifies center, radius length, and distance calculation that defines the
-		/// concept of radius, with no bounds.
+		/// Constructor.
 		/// </summary>
 		/// <param name="center">The center point of the radius.</param>
 		/// <param name="radius">The length of the radius.</param>
 		/// <param name="distanceCalc">
 		/// The distance calculation used to determine what shape the radius has (or a type
-		/// implicitly convertible to Distance, eg. Radius).
+		/// implicitly convertible to a distance method, eg. <see cref="Radius"/>).
 		/// </param>
 		public RadiusAreaProvider(Coord center, int radius, Distance distanceCalc)
 			: this(center, radius, distanceCalc, Rectangle.EMPTY) { }
 
 		/// <summary>
-		/// Constructor. Specifies center, radius length, and distance calculation that defines the
-		/// concept of radius, with no bounds.
+		/// Constructor.
 		/// </summary>
 		/// <param name="centerX">The x-value of the center point of the radius.</param>
 		/// <param name="centerY">The y-value of the center point of the radius.</param>
 		/// <param name="radius">The length of the radius.</param>
 		/// <param name="distanceCalc">
 		/// The distance calculation used to determine what shape the radius has (or a type
-		/// implicitly convertible to Distance, eg. Radius).
+		/// implicitly convertible to a distance method, eg. <see cref="Radius"/>).
 		/// </param>
 		public RadiusAreaProvider(int centerX, int centerY, int radius, Distance distanceCalc)
 			: this(new Coord(centerX, centerY), radius, distanceCalc, Rectangle.EMPTY) { }
 
 		/// <summary>
-		/// The bounds to constrain the returned Coords to. Set to Rectangle.EMPTY to indicate that
-		/// there are no bounds.
+		/// The bounds to constrain the returned locations to. Set to <see cref="Rectangle.EMPTY"/>
+		/// to indicate that there are no bounds.
 		/// </summary>
 		public Rectangle Bounds { get; set; }
 
@@ -117,7 +114,7 @@ namespace GoRogue
 
 		/// <summary>
 		/// The distance calculation used to determine what shape the radius has (or a type
-		/// implicitly convertible to Distance, eg. Radius).
+		/// implicitly convertible to a distance method, eg. <see cref="Radius"/>).
 		/// </summary>
 		public Distance DistanceCalc { get; set; }
 
@@ -147,14 +144,20 @@ namespace GoRogue
 		/// <summary>
 		/// Returns a read-only representation of this RadiusAreaProvider.
 		/// </summary>
-		/// <returns></returns>
+		/// <returns>A read-only representation of the RadiusAreaProvider</returns>
 		public IReadOnlyRadiusAreaProvider AsReadOnly() => this;
 
 		/// <summary>
-		/// Calculates the new radius, and returns an IEnumerable of all unique Coords within that
-		/// radius and bounds specified (as applicable). See class description for details on the ordering.
+		/// Calculates the new radius, and returns an IEnumerable of all unique locations within that
+		/// radius and bounds specified (as applicable).
 		/// </summary>
-		/// <returns>Enumerable of all unique Coords within the radius and bounds specified.</returns>
+		/// <remarks>
+		/// In the case that MANHATTAN/CHEBYSHEV distance, or DIAMOND/SQUARE/OCTAHEDRON/CUBE radius shapes
+		/// are specified via <see cref="DistanceCalc"/>, positions returned are guaranteed to be returned
+		/// in order of distance from the center, from least to greatest. This guarantee does NOT hold if
+		/// EUCLIDEAN distance, or CIRCLE/SPHERE radius shapes are specified.
+		/// </remarks>
+		/// <returns>Enumerable of all unique positions within the radius and bounds specified.</returns>
 		public IEnumerable<Coord> CalculatePositions()
 		{
 			for (int x = 0; x < inQueue.GetLength(0); x++)
