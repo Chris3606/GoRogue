73de05ee7f4c7f8b21e63162f28aff191f865f51
Chris Ridley <cridley106@gmail.com>
2019-04-30 14:54:07 -0500

    Updated csproj for 2.2.1 release.  Modified nuget deployment script to avoid newline issues

--- GoRogue/readme.txt
+++ GoRogue/readme.txt
@@ -1,3 +1,3 @@
-Congratulations, GoRogue has been installed!
+GoRogue has been successfully installed!  
 
 For getting started assistance, API documentation, and more, please feel free to visit https://chris3606.github.io/GoRogue.
--- nuget/make.ps1
+++ nuget/make.ps1
@@ -4,8 +4,12 @@ pushd $PSScriptRoot\..\GoRogue
 # Compile release build
 msbuild /t:pack /p:Configuration=Release
 
-# Load csproj file
+
+# Backup and load csproj file
 $fullCsProjPath = Join-Path $(Get-Location) "GoRogue.csproj"
+$fullCsprojBackupPath = Join-Path $(Get-Location) "GoRogue.csproj.bak"
+Copy-Item "$fullCsProjPath" -Destination "$fullCsprojBackupPath"
+
 [xml]$xml = Get-Content "$fullCsProjPath"
 $node = $(Select-Xml -Xml $xml -XPath '//Project/PropertyGroup/PackageVersion').Node
 
@@ -17,9 +21,9 @@ $xml.Save($fullCsProjPath)
 # Compile debug build
 msbuild /t:pack /p:Configuration=Debug
 
-# Revert version tag to release build
-$node.'#text' = $oldVersion
-$xml.Save($fullCsProjPath)
+# Revert to backup of csproj
+Remove-Item "$fullCsProjPath"
+Copy-Item "$fullCsprojBackupPath" -Destination "$fullCsProjPath"
 
 # Revert working directory to previous
 popd
