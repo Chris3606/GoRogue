2e462d754b5d45531c9ff12ffbe56072a4bb08dd
Chris3606 <cridley106@gmail.com>
2020-05-25 12:49:43 -0500

    Added performance test project, and basic baseline for map gen performance tests.

---
+++ GoRogue.PerformanceTests/MapGenDefaultAlgorithms.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using GoRogue.MapGeneration;
+
+namespace GoRogue.PerformanceTests
+{
+    public class MapGenDefaultAlgorithms
+    {
+        //private readonly Generator _generator;
+
+        //public MapGenDefaultAlgorithms()
+        //{
+            //_generator = new Generator(40, 50);
+        //}
+
+        [Benchmark]
+        public GenerationContext DungeonMazeMap()
+        {
+            var generator = new Generator(50, 40);
+            generator.AddSteps(DefaultAlgorithms.DungeonMazeMapSteps());
+
+            generator.Generate();
+            return generator.Context;
+        }
+    }
+    
+}
---
+++ GoRogue.PerformanceTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace GoRogue.PerformanceTests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            var summary = BenchmarkRunner.Run<MapGenDefaultAlgorithms>();
+        }
+	}
+}
