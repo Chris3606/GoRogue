2c12b472e3bb6e8a2529c89afb7d6a3f73f80957
Chris3606 <cridley106@gmail.com>
2020-06-29 20:05:32 -0500

    Modified dice tests to properly use theories.

--- GoRogue.UnitTests/DiceNotation/DiceNotationTests.cs
+++ GoRogue.UnitTests/DiceNotation/DiceNotationTests.cs
@@ -1,6 +1,7 @@
 ﻿using GoRogue.DiceNotation;
 using JetBrains.Annotations;
 using Xunit;
+using XUnit.ValueTuples;
 
 namespace GoRogue.UnitTests.DiceNotation
 {
@@ -13,6 +14,7 @@ namespace GoRogue.UnitTests.DiceNotation
             Assert.Equal(max, expr.MaxRoll());
         }
 
+        [AssertionMethod]
         private static void AssertReturnedInRange(IDiceExpression expr, int min, int max)
         {
             for (var i = 0; i < 100; i++)
@@ -22,84 +24,37 @@ namespace GoRogue.UnitTests.DiceNotation
             }
         }
 
-        [Fact]
-        public void AdvancedDice()
+        public static (string expr, int min, int max)[] DiceExpressions  = new[]
         {
-            var expr = Dice.Parse("1d(1d12+4)+3");
-            AssertMinMaxValues(expr, 4, 19);
-            AssertReturnedInRange(expr, 4, 19);
-        }
+            // Advanced Dice Expression
+            ("1d(1d12+4)+3", 4, 19),
+            // Keep and Add
+            ("5d6k2+3", 5, 15),
+            // Multiple dice
+            ("2d6", 2, 12),
+            // Multiple dice add
+            ("2d6+3", 5, 15),
+            // Multiple dice add and multiply
+            ("(2d6+2)*3", 12, 42),
+            // Multiple dice with multiply
+            ("3*2d6", 6, 36),
+            // Single dice
+            ("1d6", 1, 6),
+            // Single dice with add
+            ("1d6+3", 4, 9),
+            // Single dice with add and multiply
+            ("3*(1d6+2)", 9, 24),
+            // Single dice with multiply
+            ("1d6*3", 3, 18)
+        };
 
-        [Fact]
-        public void KeepDiceAdd()
+        [Theory]
+        [MemberDataTuple(nameof(DiceExpressions))]
+        public void DiceExpressionValues(string expression, int min, int max)
         {
-            var expr = Dice.Parse("5d6k2+3");
-            AssertMinMaxValues(expr, 5, 15);
-            AssertReturnedInRange(expr, 5, 15);
-        }
-
-        [Fact]
-        public void MultipleDice()
-        {
-            var expr = Dice.Parse("2d6");
-            AssertMinMaxValues(expr, 2, 12);
-            AssertReturnedInRange(expr, 2, 12);
-        }
-
-        [Fact]
-        public void MultipleDiceAdd()
-        {
-            var expr = Dice.Parse("2d6+3");
-            AssertMinMaxValues(expr, 5, 15);
-            AssertReturnedInRange(expr, 5, 15);
-        }
-
-        [Fact]
-        public void MultipleDiceAddMultiply()
-        {
-            var expr = Dice.Parse("(2d6+2)*3");
-            AssertMinMaxValues(expr, 12, 42);
-            AssertReturnedInRange(expr, 12, 42);
-        }
-
-        [Fact]
-        public void MultipleDiceMultiply()
-        {
-            var expr = Dice.Parse("3*2d6");
-            AssertMinMaxValues(expr, 6, 36);
-            AssertReturnedInRange(expr, 6, 36);
-        }
-
-        [Fact]
-        public void SingleDice()
-        {
-            var expr = Dice.Parse("1d6");
-            AssertMinMaxValues(expr, 1, 6);
-            AssertReturnedInRange(expr, 1, 6);
-        }
-
-        [Fact]
-        public void SingleDiceAdd()
-        {
-            var expr = Dice.Parse("1d6+3");
-            AssertMinMaxValues(expr, 4, 9);
-            AssertReturnedInRange(expr, 4, 9);
-        }
-
-        [Fact]
-        public void SingleDiceAddMultiply()
-        {
-            var expr = Dice.Parse("3*(1d6+2)");
-            AssertMinMaxValues(expr, 9, 24);
-            AssertReturnedInRange(expr, 9, 24);
-        }
-
-        [Fact]
-        public void SingleDiceMultiply()
-        {
-            var expr = Dice.Parse("1d6*3");
-            AssertMinMaxValues(expr, 3, 18);
-            AssertReturnedInRange(expr, 3, 18);
+            var expr = Dice.Parse(expression);
+            AssertMinMaxValues(expr, min, max);
+            AssertReturnedInRange(expr, min, max);
         }
     }
 }
