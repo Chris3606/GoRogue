7b6e1abb1c68c42b42f722cef7852a1f208823e9
Chris3606 <cridley106@gmail.com>
2020-07-04 13:10:33 -0500

    Moved component mock classes to mock namespace.

---
+++ GoRogue.UnitTests/Mocks/MockComponents.cs
@@ -0,0 +1,18 @@
+﻿namespace GoRogue.UnitTests.Mocks
+{
+    internal class ComponentBase
+    { }
+
+    internal class Component1 : ComponentBase
+    { }
+
+    internal class Component2 : ComponentBase
+    { }
+
+    internal class SortedComponent : Component1, ISortedComponent
+    {
+        public SortedComponent(uint sortOrder) => SortOrder = sortOrder;
+
+        public uint SortOrder { get; }
+    }
+}
