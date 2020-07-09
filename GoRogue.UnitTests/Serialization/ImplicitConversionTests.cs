05f818502051b3b10a5529fd3b59ab8e864b953c
Chris3606 <cridley106@gmail.com>
2020-07-09 13:24:37 -0500

    Added tests to test implicit conversions to expressive types.  Added method to test implicit conversions by theory based on data in TestData.

--- GoRogue.UnitTests/Serialization/ImplicitConversionTests.cs
+++ GoRogue.UnitTests/Serialization/ImplicitConversionTests.cs
@@ -1,17 +1,45 @@
-﻿namespace GoRogue.UnitTests.Serialization
+﻿using System.Collections.Generic;
+using Xunit;
+using XUnit.ValueTuples;
+
+namespace GoRogue.UnitTests.Serialization
 {
     public class ImplicitConversionTests
     {
-        /* ------------TEMPLATE------------------------
-        [Fact]
-        public void AreaToAreaSerialized()
-        {
-            var original = new Area(TestUtils.Enumerable<Point>((1, 2), (3, 4), (5, 6)));
-            AreaSerialized expressive = (AreaSerialized)original;
-            var converted = (Area)expressive;
+        // Required to be in-class so we just forward
+        public static IEnumerable<object> AllNonExpressiveTypes = TestData.AllNonExpressiveTypes;
 
-            Assert.Equal(original, converted);
+        [Theory]
+        [MemberDataEnumerable(nameof(AllNonExpressiveTypes))]
+        public void RegularToSerializedConversion(object original)
+        {
+            var originalType = original.GetType();
+            var serializedType = TestData.RegularToExpressiveTypes[originalType];
+            var equalityFunc = Comparisons.GetComparisonFunc(original);
+
+            // Retrieve each implicit conversion operator.  We must use reflection because there is no way to add type
+            // constraints that tell the compiler that the object must have these implicit conversions without adding
+            // unnecessary things to the GoRogue source code.  The conversion operators must be there or it's an error
+            // in the test case, so we check.
+            var toExpressive = serializedType.GetMethod("op_Implicit", new[] { originalType });
+            var toOriginal = serializedType.GetMethod("op_Implicit", new[] { serializedType });
+            // Make sure the conversion functions exist
+            TestUtils.NotNull(toExpressive);
+            TestUtils.NotNull(toOriginal);
+
+            // Convert to expressive type and assert it returns a valid object of the correct type
+            var expressive = toExpressive.Invoke(null, new[] { original });
+            TestUtils.NotNull(expressive);
+            Assert.Equal(serializedType, expressive.GetType());
+
+            // Convert from the expressive type back to the original and assert it returns a valid object
+            // of the correct type
+            var converted = toOriginal.Invoke(null, new[] { expressive });
+            TestUtils.NotNull(converted);
+            Assert.Equal(originalType, converted.GetType());
+
+            // Assert the type is equivalent after conversion as compared to before
+            Assert.True(equalityFunc(original, converted));
         }
-        */
     }
 }
--- GoRogue.UnitTests/Serialization/TestData.cs
+++ GoRogue.UnitTests/Serialization/TestData.cs
@@ -9,10 +9,13 @@ namespace GoRogue.UnitTests.Serialization
 {
     public static class TestData
     {
-        // List of expressive versions of types.  The assumptions here are:
-        //     1. All types in this list are serializable via binary serialization, and data contract serialization
-        //     2. ONLY the types in this list are serializable via binary serialization
-        //     2. All types in this list serialize to JSON objects (JObject)
+        #region Original Data
+        /// <summary>
+        /// List of expressive versions of types.  The assumptions here are:
+        ///     1. All types in this list are serializable via binary serialization, and data contract serialization
+        ///     2. ONLY the types in this list are serializable via binary serialization
+        ///     2. All types in this list serialize to JSON objects (JObject)
+        /// </summary>
         public static readonly IEnumerable<object> ExpressiveTypes = new object[]
         {
             // Not expressive types, but if these don't serialize properly to binary and JSON it will break
@@ -22,9 +25,20 @@ namespace GoRogue.UnitTests.Serialization
             // ComponentTagPair
             new ComponentTagPairSerialized { Component = new Component1() { Value = 82 }, Tag = "tag" },
             new ComponentTagPairSerialized { Component = new Component2() { Value = 81 }, Tag = null },
+            // ComponentCollection
+            new ComponentCollectionSerialized
+            {
+                Components = new List<ComponentTagPairSerialized>
+                {
+                    new ComponentTagPairSerialized { Component = new Component1(), Tag = "tag" },
+                    new ComponentTagPairSerialized { Component = new Component2(), Tag = null },
+                }
+            }
         };
 
-        // List of all non-expressive types that serialize to JSON objects (JObject)
+        /// <summary>
+        /// List of all non-expressive types that serialize to JSON objects (JObject)
+        /// </summary>
         private static readonly object[] _nonExpressiveJsonObjects =
         {
             // ComponentTagPairs
@@ -32,12 +46,10 @@ namespace GoRogue.UnitTests.Serialization
             new ComponentTagPair(new Component2() { Value = 91 }, null),
         };
 
-        // All objects that should serialize to JSON objects.  All should have entries in TypeSerializedFields
-        public static IEnumerable<object> SerializableValuesJsonObjects
-            => ExpressiveTypes.Concat(_nonExpressiveJsonObjects);
-
-        // Dictionary of object types to an unordered but complete list of fields that each object type should have
-        // serialized in its JSON object form.  All objects in SerializableValuesJsonObjects should have an entry here.
+        /// <summary>
+        /// Dictionary of object types to an unordered but complete list of fields that each object type should have
+        /// serialized in its JSON object form.  All objects in SerializableValuesJsonObjects should have an entry here.
+        /// </summary>
         public static readonly Dictionary<Type, string[]> TypeSerializedFields = new Dictionary<Type, string[]>
         {
             // Not GoRogue types, but we check them to verify they serialize as expected to ensure other tests work
@@ -46,10 +58,13 @@ namespace GoRogue.UnitTests.Serialization
             // GoRogue types
             { typeof(ComponentTagPair), new []{ "Component", "Tag" } },
             { typeof(ComponentTagPairSerialized), new []{ "Component", "Tag" } },
+            { typeof(ComponentCollectionSerialized), new []{ "Components" } },
         };
 
-        // Objects that are JSON serializable but should NOT serialize to JSON objects (instead, for example,
-        // JSON array)
+        /// <summary>
+        /// Objects that are JSON serializable but should NOT serialize to JSON objects (instead, for example,
+        /// JSON array)
+        /// </summary>
         public static readonly IEnumerable<object> SerializableValuesNonJsonObjects = new object[]
         {
             // Component collection with serializable components
@@ -61,8 +76,35 @@ namespace GoRogue.UnitTests.Serialization
             },
         };
 
-        // All JSON objects for which we can test serialization equality
-        public static IEnumerable<object> AllSerializableObjects =
+        /// <summary>
+        /// Dictionary of non-expressive types to their expressive type
+        /// </summary>
+        public static readonly Dictionary<Type, Type> RegularToExpressiveTypes = new Dictionary<Type, Type>
+        {
+            { typeof(ComponentTagPair), typeof(ComponentTagPairSerialized) },
+            { typeof(ComponentCollection), typeof(ComponentCollectionSerialized) }
+        };
+        #endregion
+
+        #region Combinatory Data
+        /// <summary>
+        /// All objects that should serialize to JSON objects.  All should have entries in TypeSerializedFields
+        /// </summary>
+        public static IEnumerable<object> SerializableValuesJsonObjects
+            => ExpressiveTypes.Concat(_nonExpressiveJsonObjects);
+
+        /// <summary>
+        /// Objects that should have expressive versions of types.  Each item must have an entry in
+        /// RegularToExpressiveTypes
+        /// </summary>
+        public static IEnumerable<object> AllNonExpressiveTypes
+            => _nonExpressiveJsonObjects.Concat(SerializableValuesNonJsonObjects);
+
+        /// <summary>
+        /// All JSON objects for which we can test serialization equality
+        /// </summary>
+        public static IEnumerable<object> AllSerializableObjects =>
             SerializableValuesJsonObjects.Concat(SerializableValuesNonJsonObjects);
+        #endregion
     }
 }
