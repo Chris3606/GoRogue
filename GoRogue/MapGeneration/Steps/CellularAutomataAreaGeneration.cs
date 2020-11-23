f9aca0f5446f77999147956f6d9ce7dc17e4699b
Chris3606 <cridley106@gmail.com>
2020-11-22 21:44:35 -0600

    Fixed cellular automata generation to count walls correctly.

--- GoRogue/MapGeneration/Steps/CellularAutomataAreaGeneration.cs
+++ GoRogue/MapGeneration/Steps/CellularAutomataAreaGeneration.cs
@@ -100,7 +100,7 @@ namespace GoRogue.MapGeneration.Steps
             int count = 0;
 
             foreach (var pos in Radius.Square.PositionsInRadius(centerPos, distance))
-                if (map.Contains(pos) && pos != centerPos && map[pos])
+                if (map.Contains(pos) && pos != centerPos && !map[pos])
                     count += 1;
 
             return count;
