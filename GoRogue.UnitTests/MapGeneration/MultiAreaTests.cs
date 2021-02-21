89c85ffc629cf66db3899c449a0974e2821ef1a3
Chris3606 <cridley106@gmail.com>
2021-02-20 19:04:47 -0600

    Added MultiArea that implements IReadOnlyArea based on a list of "sub" areas.

---
+++ GoRogue.UnitTests/MapGeneration/MultiAreaTests.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GoRogue.MapGeneration;
+using SadRogue.Primitives;
+using Xunit;
+using XUnit.ValueTuples;
+
+namespace GoRogue.UnitTests.MapGeneration
+{
+    public class MultiAreaTests
+    {
+        #region Test Data
+
+        private static readonly IEnumerable<Area> _subAreas = new[]
+        {
+            new Area((1, 2), (3, 4), (5, 6)), new Area((9, 10), (11, 12), (14, 13))
+        };
+
+        private static readonly Rectangle _boundsOutsideAreas = new Rectangle(0, 0, 50, 50);
+
+        public static readonly IEnumerable<MultiArea> Areas = new[]
+        {
+            new MultiArea(_subAreas.First()),
+            new MultiArea(_subAreas.Take(2)),
+            new MultiArea(_subAreas.First().Yield().Append(_subAreas.Last())),
+            new MultiArea(_subAreas)
+        };
+        #endregion
+
+        [Fact]
+        public void CreateEmpty()
+        {
+            var area = new MultiArea();
+
+            Assert.Empty(area.SubAreas);
+            Assert.Equal(Rectangle.Empty, area.Bounds);
+            Assert.Empty(area);
+        }
+
+        [Fact]
+        public void CreateFromSingleArea()
+        {
+            // Create area for testing
+            var subArea = new Area((1, 2), (3, 4), (5, 6));
+            var area = new MultiArea(subArea);
+
+            // Should have one sub-area
+            Assert.Single(area.SubAreas);
+            Assert.Equal(area.Count, subArea.Count);
+            Assert.Equal(subArea, area.SubAreas[0]);
+            Assert.NotEmpty(area);
+            TestUtils.Matches<IReadOnlyArea>(subArea, area);
+            TestUtils.Matches<IReadOnlyArea>(area, subArea);
+        }
+
+        [Fact]
+        public void CreateFromMultipleAreas()
+        {
+            // Create area for testing
+            var area = new MultiArea(_subAreas);
+
+            // Should have one sub-area
+            Assert.Equal(2, area.SubAreas.Count);
+            Assert.Equal(area.Count, _subAreas.Sum(a => a.Count));
+            Assert.Equal(_subAreas, area.SubAreas);
+            Assert.NotEmpty(area);
+        }
+
+        [Theory]
+        [MemberDataEnumerable(nameof(Areas))]
+        public void ContainsPoints(MultiArea area)
+        {
+            foreach (var point in _boundsOutsideAreas.Positions())
+            {
+                bool subAreasContain = area.SubAreas.Any(i => i.Contains(point));
+                Assert.Equal(subAreasContain, area.Contains(point));
+            }
+        }
+
+        [Theory]
+        [MemberDataEnumerable(nameof(Areas))]
+        public void ContainsIntPoints(MultiArea area)
+        {
+            foreach (var point in _boundsOutsideAreas.Positions())
+            {
+                bool subAreasContain = area.SubAreas.Any(i => i.Contains(point.X, point.Y));
+                Assert.Equal(subAreasContain, area.Contains(point.X, point.Y));
+            }
+        }
+
+        [Theory]
+        [MemberDataEnumerable(nameof(Areas))]
+        public void ContainsArea(MultiArea area)
+        {
+            foreach (var point in _boundsOutsideAreas.Positions())
+            {
+                var compArea = new Area(point);
+                bool subAreasContain = area.SubAreas.Any(i => i.Contains(compArea));
+                Assert.Equal(subAreasContain, area.Contains(compArea));
+            }
+        }
+
+        [Theory]
+        [MemberDataEnumerable(nameof(Areas))]
+        public void IntersectsArea(MultiArea area)
+        {
+            foreach (var point in _boundsOutsideAreas.Positions())
+            {
+                var compArea = new Area(point);
+                bool subAreasIntersect = area.SubAreas.Any(i => i.Intersects(compArea));
+                Assert.Equal(subAreasIntersect, area.Intersects(compArea));
+            }
+        }
+
+        [Theory]
+        [MemberDataEnumerable(nameof(Areas))]
+        public void GetPoint(MultiArea area)
+        {
+            var points = new HashSet<Point>(area);
+
+            // Test requires all unique areas and that all points are included in the returned areas
+            Assert.Equal(area.SubAreas.Sum(i => i.Count), points.Count);
+
+            // Check that we get the right point back, based on its calculated index
+            foreach (var point in points)
+            {
+                int index = FindIndexOf(area.SubAreas, point);
+                Assert.Equal(point, area[index]);
+            }
+        }
+
+        [Fact]
+        public void AddSubArea()
+        {
+            var area = new MultiArea();
+            Assert.Empty(area.SubAreas);
+            Assert.Equal(0, area.Count);
+            Assert.Empty(area);
+
+            int i = 1;
+            foreach (var subArea in _subAreas)
+            {
+                area.Add(subArea);
+                Assert.Equal(_subAreas.Take(i), area.SubAreas);
+                Assert.Equal(_subAreas.Take(i).Sum(a => a.Count), area.Count);
+                Assert.Equal(_subAreas.Take(i).Flatten(), area);
+
+                i++;
+            }
+        }
+
+        [Fact]
+        public void RemoveSubArea()
+        {
+            var area = new MultiArea(_subAreas);
+
+            Assert.Equal(_subAreas, area.SubAreas);
+            Assert.Equal(_subAreas.Sum(a => a.Count), area.Count);
+            Assert.Equal(_subAreas.Flatten(), area);
+
+            int i = 1;
+            foreach (var subArea in _subAreas)
+            {
+                area.Remove(subArea);
+                Assert.Equal(_subAreas.Skip(i), area.SubAreas);
+                Assert.Equal(_subAreas.Skip(i).Sum(a => a.Count), area.Count);
+                Assert.Equal(_subAreas.Skip(i).Flatten(), area);
+
+                i++;
+            }
+        }
+
+        // Returns an index of the point, where index 0 is the first index
+        // of the first area, index area1.Count is the first index of the second
+        // area, etc (mimicking the functionality of MultiArea's indexer in a more
+        // primitive way)
+        private static int FindIndexOf(IEnumerable<IReadOnlyArea> areas, Point point)
+        {
+            int sum = 0;
+            foreach (var area in areas)
+            {
+                foreach (var p in area)
+                {
+                    if (p == point) return sum;
+                    sum++;
+                }
+            }
+
+            throw new ArgumentException("Cannot find point in any of the given areas.", nameof(point));
+        }
+    }
+}
---
+++ GoRogue/MapGeneration/MultiArea.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using SadRogue.Primitives;
+
+namespace GoRogue.MapGeneration
+{
+    /// <summary>
+    /// A class implementing the <see cref="IReadOnlyArea"/> interface, that derives its area from multiple
+    /// "sub-areas".
+    /// </summary>
+    [PublicAPI]
+    public class MultiArea : IReadOnlyArea
+    {
+        private readonly List<IReadOnlyArea> _subAreas;
+
+        /// <summary>
+        /// List of all sub-areas in the MultiArea.
+        /// </summary>
+        public IReadOnlyList<IReadOnlyArea> SubAreas => _subAreas.AsReadOnly();
+
+        // TODO: Modify to be in ExpandToFit function in Rectangle
+        /// <inheritdoc/>
+        public Rectangle Bounds
+        {
+            get
+            {
+                if (_subAreas.Count == 0) return Rectangle.Empty;
+
+                var firstBounds = _subAreas[0].Bounds;
+                int minX = firstBounds.MinExtentX;
+                int minY = firstBounds.MinExtentY;
+                int maxX = firstBounds.MaxExtentX;
+                int maxY = firstBounds.MaxExtentY;
+
+                for (int i = 1; i < _subAreas.Count; i++)
+                {
+                    var currentBounds = _subAreas[i].Bounds;
+                    if (minX > currentBounds.MinExtentX) minX = currentBounds.MinExtentX;
+                    if (minY > currentBounds.MinExtentY) minY = currentBounds.MinExtentY;
+                    if (maxX < currentBounds.MaxExtentX) maxX = currentBounds.MaxExtentX;
+                    if (maxY < currentBounds.MaxExtentY) maxY = currentBounds.MaxExtentY;
+                }
+
+                return new Rectangle(new Point(minX, minY), new Point(maxX, maxY));
+            }
+        }
+
+        /// <inheritdoc/>
+        public int Count => _subAreas.Sum(area => area.Count);
+
+        /// <inheritdoc/>
+        public Point this[int index]
+        {
+            get
+            {
+                int sum = 0;
+                foreach (var area in _subAreas)
+                {
+                    if (sum + area.Count > index)
+                        return area[index - sum];
+
+                    sum += area.Count;
+                }
+
+                throw new ArgumentOutOfRangeException(nameof(index), "Index given is not valid.");
+            }
+        }
+
+        /// <summary>
+        /// Creates an area with no points/subareas.
+        /// </summary>
+        public MultiArea()
+        {
+            _subAreas = new List<IReadOnlyArea>();
+        }
+
+        /// <summary>
+        /// Creates a MultiArea that has only the given sub-area.
+        /// </summary>
+        /// <param name="area">Sub-area to add.</param>
+        public MultiArea(IReadOnlyArea area)
+            : this(area.Yield())
+        { }
+
+        /// <summary>
+        /// Creates a multi-area that is comprised of the given sub-areas.
+        /// </summary>
+        /// <param name="areas">Sub-areas to add.</param>
+        public MultiArea(IEnumerable<IReadOnlyArea> areas) => _subAreas = new List<IReadOnlyArea>(areas);
+
+        /// <summary>
+        /// Adds the given sub-area to the MultiArea.
+        /// </summary>
+        /// <param name="subArea">The sub-area to add.</param>
+        public void Add(IReadOnlyArea subArea) => _subAreas.Add(subArea);
+
+        /// <summary>
+        /// Removes the given sub-area from the MultiArea.
+        /// </summary>
+        /// <param name="subArea">The sub-area to remove.</param>
+        public void Remove(IReadOnlyArea subArea) => _subAreas.Remove(subArea);
+
+        // TODO: Make this in the primitives lib an extension method or default interface method; it's a copy-paste of Area.
+        /// <summary>
+        /// Compares for equality. Returns true if the two areas contain exactly the same points.
+        /// </summary>
+        /// <param name="other"/>
+        /// <returns>True if the areas contain exactly the same points, false otherwise.</returns>
+        public bool Matches(IReadOnlyArea? other)
+        {
+            if (other is null)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            // Quick checks that can short-circuit a function that would otherwise require looping over all points
+            if (Count != other.Count)
+                return false;
+
+            if (Bounds != other.Bounds)
+                return false;
+
+            foreach (Point pos in this)
+                if (!other.Contains(pos))
+                    return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through all positions in all sub-areas.
+        /// </summary>
+        /// <returns>An enumerator that iterates through all positions in all sub-areas.</returns>
+        public IEnumerator<Point> GetEnumerator()
+        {
+            foreach (var area in _subAreas)
+            {
+                foreach (var point in area)
+                    yield return point;
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through all positions in all sub-areas.
+        /// </summary>
+        /// <returns>An enumerator that iterates through all positions in all sub-areas.</returns>
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        /// <inheritdoc/>
+        public bool Contains(IReadOnlyArea area)
+        {
+            foreach (var subArea in _subAreas)
+                if (subArea.Contains(area))
+                    return true;
+
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public bool Contains(Point position)
+        {
+            foreach (var subArea in _subAreas)
+                if (subArea.Contains(position))
+                    return true;
+
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public bool Contains(int positionX, int positionY)
+        {
+            foreach (var subArea in _subAreas)
+                if (subArea.Contains(positionX, positionY))
+                    return true;
+
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public bool Intersects(IReadOnlyArea area)
+        {
+            foreach (var subArea in _subAreas)
+                if (subArea.Intersects(area))
+                    return true;
+
+            return false;
+        }
+    }
+}
