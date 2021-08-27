9f8d8c3514db2fcf637955d725f6e6a96c275d84
Forrest Cheadle <fcheadle@gmail.com>
2021-08-27 17:11:42 -0600

    added new tests

--- GoRogue.UnitTests/MapGeneration/PolygonAreaTests.cs
+++ GoRogue.UnitTests/MapGeneration/PolygonAreaTests.cs
@@ -8,24 +8,6 @@ namespace GoRogue.UnitTests.MapGeneration
 {
     public class PolygonAreaTests
     {
-        /*
-         *  0 1 2 3 4 5 6 7 8
-         * 0# - - - - #
-         * 1- . . . . -
-         * 2- . . . . -
-         * 3- . . . . -
-         * 4- . . . . -
-         * 5- . . . . -
-         * 6- . . . . -
-         * 7- . . . . -
-         * 8# - - - - #
-         */
-        private PolygonArea _rectangle;
-        private readonly Point _rectNorthWest = (0, 0);
-        private readonly Point _rectNorthEast = (5, 0);
-        private readonly Point _rectSouthWest = (0, 8);
-        private readonly Point _rectSouthEast = (5, 8);
-
         /* (roughly)
          *  0 1 2 3 4 5 6 7 8
          * 0# - -
@@ -38,18 +20,57 @@ namespace GoRogue.UnitTests.MapGeneration
          * 7   -.-
          * 8    #
          */
-        private PolygonArea _triangle;
         private readonly Point _triPointOne = (0, 0);
         private readonly Point _triPointTwo = (8, 2);
         private readonly Point _triPointThree = (2, 8);
 
+        /*
+         *  0 1 2 3 4 5 6 7 8
+         * 0# - - - - #
+         * 1- . . . . -
+         * 2- . . . . -
+         * 3- . . . . -
+         * 4- . . . . -
+         * 5- . . . . -
+         * 6- . . . . -
+         * 7- . . . . -
+         * 8# - - - - #
+         */
+        private readonly Point _rectNorthWest = (0, 0);
+        private readonly Point _rectNorthEast = (5, 0);
+        private readonly Point _rectSouthWest = (0, 8);
+        private readonly Point _rectSouthEast = (5, 8);
 
-        public PolygonAreaTests()
+        /*
+         * Hand-Calculated
+         */
+        private readonly Point _pentPointOne = (0, 4);
+        private readonly Point _pentPointTwo = (8, 3);
+        private readonly Point _pentPointThree = (7, 8);
+        private readonly Point _pentPointFour = (2, 8);
+        private readonly Point _pentPointFive = (0, 3);
+
+        /*
+         * Hand-Calculated
+         */
+        private readonly Point _hexPointOne = (9, 9);
+        private readonly Point _hexPointTwo = (12, 11);
+        private readonly Point _hexPointThree = (12, 14);
+        private readonly Point _hexPointFour = (9, 16);
+        private readonly Point _hexPointFive = (6, 14);
+        private readonly Point _hexPointSix = (6, 11);
+
+        /*
+         * Hand-Calculated
+         */
+        private readonly Point[] _initialCorners = new[]
         {
-            _rectangle = new PolygonArea(Lines.Algorithm.DDA, _rectNorthEast, _rectNorthWest, _rectSouthWest, _rectSouthEast);
-            _triangle = new PolygonArea(Lines.Algorithm.DDA, _triPointOne, _triPointTwo, _triPointThree);
-        }
-
+            new Point(1,1),new Point(6,1),new Point(6,3),new Point(4,3),new Point(5,6),
+            new Point(5,7),new Point(4,7),new Point(4,12),new Point(13,12),new Point(10,11),
+            new Point(8,9),new Point(7,7),new Point(7,6),new Point(8,4),new Point(10,2),
+            new Point(13,1),new Point(15,2),new Point(16,4),new Point(11,4),new Point(10,7),
+            new Point(11,9),new Point(16,9),new Point(15,11),new Point(15,13),new Point(1,13),
+        };
 
         public static readonly IEnumerable<object[]> OrderedAlgorithms = new List<object[]>()
         {
@@ -63,15 +84,50 @@ namespace GoRogue.UnitTests.MapGeneration
         };
 
         [Fact]
-        public void PolygonAreaTest()
+        public void TriangularPolygonAreaTest()
         {
-            Assert.Equal(4, _rectangle.Corners.Count);
-            Assert.Equal(30, _rectangle.OuterPoints.Count);
-            Assert.Equal(28, _rectangle.InnerPoints.Count);
+            var triangle = new PolygonArea(Lines.Algorithm.DDA, _triPointOne, _triPointTwo, _triPointThree);
+            Assert.Equal(3, triangle.Corners.Count);
+            Assert.Equal(25, triangle.OuterPoints.Count);
+            Assert.Equal(20, triangle.InnerPoints.Count);
+        }
 
-            Assert.Equal(3, _triangle.Corners.Count);
-            Assert.Equal(25, _triangle.OuterPoints.Count);
-            Assert.Equal(20, _triangle.InnerPoints.Count);
+        [Fact]
+        public void RectangularPolygonAreaTest()
+        {
+            var rectangle = new PolygonArea(Lines.Algorithm.DDA, _rectNorthEast, _rectNorthWest, _rectSouthWest, _rectSouthEast);
+            Assert.Equal(4, rectangle.Corners.Count);
+            Assert.Equal(30, rectangle.OuterPoints.Count);
+            Assert.Equal(28, rectangle.InnerPoints.Count);
+        }
+
+        [Fact]
+        public void PentagonalPolygonAreaTest()
+        {
+            var pentagon = new PolygonArea(Lines.Algorithm.DDA, _pentPointOne, _pentPointTwo, _pentPointThree,
+                _pentPointFour, _pentPointFive);
+            Assert.Equal(5, pentagon.Corners.Count);
+            Assert.Equal(29, pentagon.OuterPoints.Count);
+            Assert.Equal(18, pentagon.InnerPoints.Count);
+        }
+
+        [Fact]
+        public void HexagonalPolygonAreaTest()
+        {
+            var hexagon = new PolygonArea(Lines.Algorithm.DDA, _hexPointOne, _hexPointTwo, _hexPointThree, _hexPointFour,
+                _hexPointFive, _hexPointSix);
+            Assert.Equal(6, hexagon.Corners.Count);
+            Assert.Equal(24, hexagon.OuterPoints.Count);
+            Assert.Equal(22, hexagon.InnerPoints.Count);
+        }
+
+        [Fact]
+        public void ComplexPolygonAreaTest()
+        {
+            var initials = new PolygonArea(_initialCorners);
+            Assert.Equal(25, initials.Corners.Count);
+            Assert.Equal(117, initials.OuterPoints.Count);
+            Assert.Equal(56, initials.InnerPoints.Count);
         }
 
         [Theory]
@@ -101,7 +157,6 @@ namespace GoRogue.UnitTests.MapGeneration
             var p3 = new Point(10, 7);
 
             Assert.Throws<ArgumentException>(() => new PolygonArea(algorithm, p1, p2, p3));
-
         }
     }
 }
