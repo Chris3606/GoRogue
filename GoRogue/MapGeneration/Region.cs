df041eb19653f986fed95a5396e109385c38b210
Forrest Cheadle <fcheadle@gmail.com>
2021-10-01 17:56:41 -0600

    Added AreaChanged events and handler, removed pseudo-implementation of some PolygonArea methods on Region.

--- GoRogue/MapGeneration/Region.cs
+++ GoRogue/MapGeneration/Region.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +11,54 @@ using SadRogue.Primitives;
 namespace GoRogue.MapGeneration
 {
     /// <summary>
+    /// Event fired when a region's Area is changed.
+    /// </summary>
+    [PublicAPI]
+    public class RegionAreaChanged : EventArgs
+    {
+        /// <summary>
+        /// The region whose area is changing
+        /// </summary>
+        public readonly Region Item;
+
+        /// <summary>
+        /// The previous Area
+        /// </summary>
+        public readonly Area Old;
+
+        /// <summary>
+        /// The Area to which we update
+        /// </summary>
+        public readonly Area New;
+
+        /// <summary>
+        /// The Event Arguments for when a region's area is changed
+        /// </summary>
+        /// <param name="item">The region whose area is changing</param>
+        /// <param name="oldArea">The former value of the Area</param>
+        /// <param name="newArea">The new value of the Area</param>
+        public RegionAreaChanged(Region item, Area oldArea, Area newArea)
+        {
+            Item = item;
+            Old = oldArea;
+            New = newArea;
+        }
+    }
+    /// <summary>
     /// A region of the map with four sides of arbitrary shape and size
     /// </summary>
     [PublicAPI]
-    public class Region : IObjectWithComponents, IReadOnlyArea
+    public class Region : IObjectWithComponents
     {
         /// <summary>
         /// The Area of this region
         /// </summary>
         public PolygonArea Area { get; set; }
+
+        /// <summary>
+        /// Fired when the Area is changed.
+        /// </summary>
+        public EventHandler<RegionAreaChanged>? AreaChanged;
 
         /// <inheritdoc/>
         public IComponentCollection GoRogueComponents { get; }
@@ -72,62 +112,24 @@ namespace GoRogue.MapGeneration
         /// <param name="corners">The corners of the region</param>
         public Region(params Point[] corners) : this(corners, Lines.Algorithm.DDA) { }
 
+        /// <summary>
+        /// Returns a new Region using the provided area
+        /// </summary>
+        /// <param name="area">This region's Area</param>
+        /// <param name="components">A component collection for this region</param>
+        public Region(PolygonArea area, IComponentCollection? components = null)
+        {
+            Area = area;
+            GoRogueComponents = components ?? new ComponentCollection();
+            GoRogueComponents.ParentForAddedComponents = this;
+        }
+
         private Region(List<Point> corners, Lines.Algorithm algorithm, IComponentCollection? components)
         {
             Area = new PolygonArea(ref corners, algorithm);
             GoRogueComponents = components ?? new ComponentCollection();
             GoRogueComponents.ParentForAddedComponents = this;
         }
-
-        #region Properties
-
-        /// <summary>
-        /// The left-most X-value of the region's four corners
-        /// </summary>
-        public int Left => Area.Left;
-
-        /// <summary>
-        /// The right-most X-value of the region's four corners
-        /// </summary>
-        public int Right => Area.Right;
-
-        /// <summary>
-        /// The top-most Y-value of the region's four corners
-        /// </summary>
-        public int Top => Area.Top;
-
-        /// <summary>
-        /// The bottom-most Y-value of the region's four corners
-        /// </summary>
-        public int Bottom => Area.Bottom;
-
-        /// <summary>
-        /// How Wide this region is
-        /// </summary>
-        public int Width => Area.Width;
-
-        /// <summary>
-        /// how tall this region is
-        /// </summary>
-        public int Height => Area.Height;
-
-        /// <summary>
-        /// The Center point of this region
-        /// </summary>
-        public Point Center => Area.Center;
-
-        /// <inheritdoc />
-        public Rectangle Bounds => Area.Bounds;
-
-        /// <inheritdoc />
-        public int Count => Area.Count;
-
-        /// <inheritdoc />
-        public Point this[int index] => Area[index];
-        #endregion
-
-        //Functions to access information about regions
-        #region Data Access Functions
 
         /// <summary>
         /// Returns a string detailing the region's corner locations.
@@ -139,61 +141,5 @@ namespace GoRogue.MapGeneration
             answer.Append(Area);
             return answer.ToString();
         }
-
-        /// <summary>
-        /// Is this Point one of the corners of the Region?
-        /// </summary>
-        /// <param name="position">the point to evaluate</param>
-        public bool IsCorner(Point position) => Area.IsCorner(position);
-
-        /// <summary>
-        /// The value of the left-most Point in the region at elevation y
-        /// </summary>
-        /// <param name="y">The elevation to evaluate</param>
-        /// <returns>The X-value of a Point</returns>
-        public int LeftAt(int y) => Area.LeftAt(y);
-
-        /// <summary>
-        /// The value of the right-most Point in the region at elevation y
-        /// </summary>
-        /// <param name="y">The elevation to evaluate</param>
-        /// <returns>The X-value of a Point</returns>
-        public int RightAt(int y) => Area.RightAt(y);
-
-        /// <summary>
-        /// The value of the top-most Point in the region at longitude x
-        /// </summary>
-        /// <param name="x">The longitude to evaluate</param>
-        /// <returns>The Y-value of a Point</returns>
-        public int TopAt(int x) => Area.TopAt(x);
-
-        /// <summary>
-        /// The value of the bottom-most Point in the region at longitude x
-        /// </summary>
-        /// <param name="x">The longitude to evaluate</param>
-        /// <returns>The Y-value of a Point</returns>
-        public int BottomAt(int x) => Area.BottomAt(x);
-
-        /// <inheritdoc />
-        public bool Matches(IReadOnlyArea? area) => Area.Matches(area);
-
-        /// <inheritdoc />
-        public bool Contains(IReadOnlyArea area) => Area.Contains(area);
-
-        /// <inheritdoc />
-        public bool Contains(Point position) => Area.Contains(position);
-
-        /// <inheritdoc />
-        public bool Contains(int positionX, int positionY) => Contains((positionX, positionY));
-
-        /// <inheritdoc />
-        public bool Intersects(IReadOnlyArea area) => Area.Intersects(area);
-
-        /// <inheritdoc />
-        public IEnumerator<Point> GetEnumerator() => Area.GetEnumerator();
-
-        /// <inheritdoc />
-        IEnumerator IEnumerable.GetEnumerator() => Area.GetEnumerator();
-        #endregion
     }
 }
