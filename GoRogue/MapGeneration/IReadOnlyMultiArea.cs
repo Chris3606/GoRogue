1e569c084ea6996b7128f04502556ec9741562d0
Forrest Cheadle <fcheadle@gmail.com>
2021-09-30 20:44:51 -0600

    added documentation to IReadOnlyMultiArea, recanted plea for forgiveness in code comment

--- GoRogue/MapGeneration/IReadOnlyMultiArea.cs
+++ GoRogue/MapGeneration/IReadOnlyMultiArea.cs
@@ -3,8 +3,14 @@ using SadRogue.Primitives;
 
 namespace GoRogue.MapGeneration
 {
+    /// <summary>
+    /// A IReadOnlyArea with SubAreas
+    /// </summary>
     public interface IReadOnlyMultiArea : IReadOnlyArea
     {
+        /// <summary>
+        /// The Sub-Areas which have been added to this Area
+        /// </summary>
         IReadOnlyList<IReadOnlyArea> SubAreas { get; }
     }
 }
