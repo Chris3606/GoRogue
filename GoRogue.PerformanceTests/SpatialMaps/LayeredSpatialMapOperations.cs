dc9b480690734913d15be9df1fd090d92e502a31
Chris3606 <cridley106@gmail.com>
2022-02-20 11:11:16 -0600

    Added MoveAll and MoveValid performance tests for SpatialMap.  Implemented equivalent performance tests for LayeredSpatialMap.

--- GoRogue.PerformanceTests/SpatialMaps/LayeredSpatialMapOperations.cs
+++ GoRogue.PerformanceTests/SpatialMaps/LayeredSpatialMapOperations.cs
@@ -6,34 +6,70 @@ using SadRogue.Primitives;
 
 namespace GoRogue.PerformanceTests.SpatialMaps
 {
-    public class SpatialMapOperations
+    /// <summary>
+    /// Basic benchmarks for LayeredSpatialMap, roughly equivalent to the benchmarks for other spatial map implementations.
+    /// </summary>
+    /// <remarks>
+    /// These benchmarks make all layers MultiSpatialMaps (eg. support multiple items), to represent a more or less
+    /// "worst case" for memory usage performance of many functions, and secondarily to replicate what is found by
+    /// default in GoRogue's Map.  There is some benefit to testing SpatialMap as well (to see how much the overhead
+    /// of LayeredSpatialMap affects an underlying layer implementation that takes less overall time than MultiSpatialMap,
+    /// however for simplicity, these tests ignore that case.
+    /// </remarks>
+    public class LayeredSpatialMapOperations
     {
         private readonly Point _initialPosition = (0, 1);
         private readonly Point _moveToPosition = (5, 6);
         private readonly Point _addPosition = (1, 1);
-        private readonly IDObject _addedObject = new IDObject();
-        private readonly IDObject _trackedObject = new IDObject();
+        private readonly IDLayerObject _addedObject = new(0);
+        private readonly IDLayerObject _trackedObject = new(0);
         private readonly int _width = 10;
-        private SpatialMap<IDObject> _testMap = null!;
+        private LayeredSpatialMap<IDLayerObject> _testMap = null!;
 
         [UsedImplicitly]
         [Params(1, 10, 50, 100)]
         public int NumEntities;
 
-        [GlobalSetup]
-        public void GlobalSetup()
+        [UsedImplicitly]
+        [Params(1, 2, 3)]
+        public int NumLayers;
+
+        [GlobalSetup(Targets = new []{nameof(MoveTwice), nameof(TryMoveTwiceOriginal), nameof(TryMoveTwice),nameof(AddAndRemove), nameof(TryAddAndRemoveOriginal), nameof(TryAddAndRemove)})]
+        public void GlobalSetupObjectsAtMoveToLocation()
         {
-            _testMap = new SpatialMap<IDObject> { { _trackedObject, _initialPosition } };
+            _testMap = new LayeredSpatialMap<IDLayerObject>(NumLayers, layersSupportingMultipleItems: uint.MaxValue) { { _trackedObject, _initialPosition } };
 
-            // Put other entities on the map, steering clear of the three points we need to remain clear to support
-            // benchmarked adds/removes.
-            int idx = -1;
-            while (_testMap.Count < NumEntities)
+            // Put other entities on the map (on each layer)
+            for (int i = 0; i < NumLayers; i++)
             {
-                idx += 1;
+                int idx = -1;
+                var layer = _testMap.GetLayer(i);
+                while (layer.Count < NumEntities)
+                {
+                    idx += 1;
+                    _testMap.Add(new IDLayerObject(i), Point.FromIndex(idx, _width));
+                }
+            }
 
-                if (idx == _initialPosition.ToIndex(_width) || idx == _moveToPosition.ToIndex(_width) || idx == _addPosition.ToIndex(_width)) continue;
-                _testMap.Add(new IDObject(), Point.FromIndex(idx, _width));
+        }
+
+        [GlobalSetup(Targets = new []{nameof(MoveAllTwice), nameof(MoveValidTwice)})]
+        public void GlobalSetupNoObjectsAtMoveToLocation()
+        {
+            _testMap = new LayeredSpatialMap<IDLayerObject>(NumLayers, layersSupportingMultipleItems: uint.MaxValue) { { _trackedObject, _initialPosition } };
+
+            // Put other entities on the map, avoiding the starting point (on each layer)
+            for (int i = 0; i < NumLayers; i++)
+            {
+                int idx = -1;
+                var layer = _testMap.GetLayer(i);
+                while (layer.Count < NumEntities)
+                {
+                    idx += 1;
+                    var point = Point.FromIndex(idx, _width);
+                    if (point != _moveToPosition)
+                        _testMap.Add(new IDLayerObject(i), Point.FromIndex(idx, _width));
+                }
             }
         }
 
@@ -77,7 +113,7 @@ namespace GoRogue.PerformanceTests.SpatialMaps
         }
 
         [Benchmark]
-        public (List<IDObject> l1, List<IDObject> l2) MoveValidTwice()
+        public (List<IDLayerObject> l1, List<IDLayerObject> l2) MoveValidTwice()
         {
             var list1 = _testMap.MoveValid(_initialPosition, _moveToPosition);
             // Move it back to not spoil next benchmark.  Valid since the GlobalSetup function used for this benchmark
