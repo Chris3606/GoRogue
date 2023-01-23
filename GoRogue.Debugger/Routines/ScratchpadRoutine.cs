1088c755d19e1bf3db004378c0f54d6ef01d1afb
Chris3606 <cridley106@gmail.com>
2023-01-22 20:42:27 -0600

    Added a "scratchpad" routine intended to be used to visually test things.

---
+++ GoRogue.Debugger/Routines/ScratchpadRoutine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using SadRogue.Primitives.GridViews;
+
+namespace GoRogue.Debugger.Routines
+{
+    public class ScratchpadRoutine : IRoutine
+    {
+
+        public string Name => "Scratchpad Routine";
+
+        private readonly List<(string name, IGridView<char> view)> _views = new List<(string name, IGridView<char> view)>();
+
+        public IReadOnlyList<(string name, IGridView<char> view)> Views => _views.AsReadOnly();
+
+        public void NextTimeUnit()
+        { }
+
+        public void LastTimeUnit()
+        { }
+
+        public void GenerateMap()
+        {
+        }
+
+        public void CreateViews()
+        { }
+
+        public void InterpretKeyPress(int key) { }
+    }
+}
