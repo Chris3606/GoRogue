73a305f3efe0f39e24a8ef5f6d914f17951f8a40
Chris3606 <cridley106@gmail.com>
2023-01-31 13:05:34 -0600

    Code cleanup.

--- GoRogue.UnitTests/LinesTests.cs
+++ GoRogue.UnitTests/LinesTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using SadRogue.Primitives;
 using Xunit;
 
@@ -8,51 +6,45 @@ namespace GoRogue.UnitTests
 {
     public class LineTests
     {
-        private static readonly (int, int) _end = (8, 6);
-        private const int _mapHeight = 10;
-        private const int _mapWidth = 10;
-        private static readonly (int, int) _start = (1, 1);
-        List<Point> _hardCodedRange = new List<Point>();
+        private readonly List<Point> _hardCodedRange = new List<Point>();
 
-        private readonly Point sw = new Point(3, 4);
-        private readonly Point nw = new Point(1, 1);
-        private readonly Point ne = new Point(5, 0);
-        private readonly Point se = new Point(7, 3);
-
-        private readonly Rectangle _orderingTests = new Rectangle(1, 2, 20, 15);
+        private readonly Point _sw = new Point(3, 4);
+        private readonly Point _nw = new Point(1, 1);
+        private readonly Point _ne = new Point(5, 0);
+        private readonly Point _se = new Point(7, 3);
 
         public LineTests()
         {
-            _hardCodedRange.AddRange(SadRogue.Primitives.Lines.GetLine(nw, ne));
-            _hardCodedRange.AddRange(SadRogue.Primitives.Lines.GetLine(ne, se));
-            _hardCodedRange.AddRange(SadRogue.Primitives.Lines.GetLine(se, sw));
-            _hardCodedRange.AddRange(SadRogue.Primitives.Lines.GetLine(sw, nw));
+            _hardCodedRange.AddRange(SadRogue.Primitives.Lines.GetLine(_nw, _ne));
+            _hardCodedRange.AddRange(SadRogue.Primitives.Lines.GetLine(_ne, _se));
+            _hardCodedRange.AddRange(SadRogue.Primitives.Lines.GetLine(_se, _sw));
+            _hardCodedRange.AddRange(SadRogue.Primitives.Lines.GetLine(_sw, _nw));
         }
 
         [Fact]
         public void LeftAtTest()
         {
-            Assert.Equal(nw.X, _hardCodedRange.LeftAt(nw.Y));
+            Assert.Equal(_nw.X, _hardCodedRange.LeftAt(_nw.Y));
             Assert.Equal(3, _hardCodedRange.LeftAt(0));
             Assert.Equal(3, _hardCodedRange.LeftAt(4));
         }
         [Fact]
         public void RightAtTest()
         {
-            Assert.Equal(ne.X, _hardCodedRange.RightAt(ne.Y));
-            Assert.Equal(se.X, _hardCodedRange.RightAt(se.Y));
+            Assert.Equal(_ne.X, _hardCodedRange.RightAt(_ne.Y));
+            Assert.Equal(_se.X, _hardCodedRange.RightAt(_se.Y));
         }
         [Fact]
         public void TopAtTest()
         {
-            Assert.Equal(ne.Y, _hardCodedRange.TopAt(ne.X));
-            Assert.Equal(nw.Y, _hardCodedRange.TopAt(nw.X));
+            Assert.Equal(_ne.Y, _hardCodedRange.TopAt(_ne.X));
+            Assert.Equal(_nw.Y, _hardCodedRange.TopAt(_nw.X));
         }
         [Fact]
         public void BottomAtTest()
         {
-            Assert.Equal(se.Y, _hardCodedRange.BottomAt(se.X));
-            Assert.Equal(sw.Y, _hardCodedRange.BottomAt(sw.X));
+            Assert.Equal(_se.Y, _hardCodedRange.BottomAt(_se.X));
+            Assert.Equal(_sw.Y, _hardCodedRange.BottomAt(_sw.X));
         }
     }
 }
