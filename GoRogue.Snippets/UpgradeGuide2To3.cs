6ac15bad5ae4cbe1c45ed2446426745156c6c9fe
Chris3606 <cridley106@gmail.com>
2023-07-06 12:28:25 -0500

    Added snippets file for upgrade guide.

---
+++ GoRogue.Snippets/UpgradeGuide2To3.cs
@@ -0,0 +1,22 @@
+﻿using SadRogue.Primitives;
+
+namespace GoRogue.Snippets
+{
+    public static class UpgradeGuide2To3
+    {
+        public static void ExampleCode()
+        {
+            #region CustomEnumerators
+            var rect = new Rectangle(1, 2, 3, 4);
+
+            // Positions() returns a RectanglePositionsEnumerator, but you can use it exactly as if it returned
+            // IEnumerable
+            foreach (var pos in rect.Positions())
+                Console.WriteLine(pos);
+
+            // You can even use it with LINQ
+            var positions = rect.Positions().ToArray();
+            #endregion
+        }
+    }
+}
