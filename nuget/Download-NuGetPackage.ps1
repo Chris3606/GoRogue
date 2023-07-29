49177b553b7ea875a6a12c1ffe6182f5ec48cd5e
Chris3606 <cridley106@gmail.com>
2023-07-29 16:20:18 -0500

    Renamed and documented PowerShell script.

--- nuget/Download-NuGetPackage.ps1
+++ nuget/Download-NuGetPackage.ps1
@@ -1,3 +1,31 @@
+<#
+.SYNOPSIS
+    Downloads the specified package from NuGet, along with all of its dependencies, then copies
+    the DLL and XML files for that package and all of its dependencies to the specified output folder.
+
+.DESCRIPTION
+    The purpose of this script is to use NuGet to resolve a package and its dependencies, and take
+    the DLLs and XML files for that package and its dependencies, and copy them to an output directory.
+
+.PARAMETER PackageName
+The name of the NuGet package to download.
+
+.PARAMETER PackageVersion
+The version of the NuGet package to download.
+
+.PARAMETER OutputFolder
+The folder where the DLLs and XML documentation files will be placed.
+
+.PARAMETER Targets
+    Targets to use to resolve packages (in order).  The first target for which each package has a DLL will be used.
+
+.EXAMPLE
+.\Download-NuGetPackage.ps1 -PackageName "Newtonsoft.Json" -PackageVersion "13.0.1" -OutputFolder "C:\Temp\Newtonsoft.Json" -Targets netstandard2.1,netstandard2.0
+
+.NOTES
+This script requires the dotnet CLI to be installed.  Additionally, it will only work for packages
+which support .NET Standard 2.1.
+#>
 param (
     [Parameter(Mandatory = $true)]
     [string]$PackageName,
@@ -6,10 +34,11 @@ param (
     [string]$PackageVersion,
 
     [Parameter(Mandatory = $true)]
-    [string]$OutputFolder
-)
+    [string]$OutputFolder,
 
-$targets = @("netstandard2.1", "netstandard2.0", "netstandard1.0")
+    [Parameter(Mandatory = $true)]
+    [string[]]$Targets
+)
 
 # Create a temporary directory we can use for the output
 $tempDir = New-Item -ItemType Directory -Path "$($ENV:Temp)\nuget_output_temp"
@@ -18,7 +47,7 @@ $tempPath = $tempDir.FullName
 try {
     # Turn temp folder into a dotnet package so we can use NuGet
     Write-Output "Creating temporary class library to use for NuGet package install..."
-    dotnet new classlib -f netstandard2.1 -o $tempPath | Out-Null
+    dotnet new classlib -f $netstandard2.1 -o $tempPath | Out-Null
 
     # Get package and all its dependencies and place them in the temp directory
     Write-Output "Installing NuGet package and all dependencies..."
@@ -32,7 +61,7 @@ try {
         
         # Find one that matches the target chain and copy it
         $foundTarget = $false
-        foreach ($target in $targets) {
+        foreach ($target in $Targets) {
             foreach ($dll in $dlls) {     
                 if (-not ($dll.FullName.Contains($target))) { continue }
 
